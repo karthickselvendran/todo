d7b5f8d050d44a7372fe4991bd3c0f12ab6c2ec8
Karthick S <selvendrankarthick@gmail.com>
2020-01-18 16:28:40 +0530

    Updates

---
+++ precache-manifest.eb10a28756214e8291022813563c8eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93dfacd8ca05813cd543c8ffd6c44415",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4f545aa740b01923faca",
+    "url": "/todo/static/css/main.a71d3f7a.chunk.css"
+  },
+  {
+    "revision": "766ab4d11707ec69a9aa",
+    "url": "/todo/static/js/2.f7f89c58.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo/static/js/2.f7f89c58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f545aa740b01923faca",
+    "url": "/todo/static/js/main.11f89862.chunk.js"
+  },
+  {
+    "revision": "41f03f1135ed8827734b",
+    "url": "/todo/static/js/runtime-main.52b113ec.js"
+  }
+]);
